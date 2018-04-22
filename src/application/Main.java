7558f3168aff7a3b3dccde7e6a126935985cce4b
szhang399 <szhang399@wisc.edu>
2018-04-22 16:49:06 -0500

    1

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,17 @@
 package application;
-	
+
 import javafx.scene.control.Button;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
 
 
 public class Main extends Application {
 	Button button;
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -19,28 +20,29 @@ public class Main extends Application {
 		primaryStage.setTitle("Tournament");
 		button = new Button();
 		button.setText("Submit");
-		
+
 		StackPane bracket_layout = new StackPane();
 		bracket_layout.getChildren().add(button);
-		
+
 		Scene scene = new Scene(bracket_layout,1200,700);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 	}
-	
+
 	/**
-     * Simplifies creating lines
-     * @param line
-     * @param x0- starting x
-     * @param y0- starting y
-     * @param x1- ending x
-     * @param y1- ending y
-     */
-    public static void initLine(Line line, Float x0, Float y0, Float x1, Float y1)
-    {
-        line.setStartX(x0);
-        line.setStartY(y0);
-        line.setEndX(x1);
-        line.setEndY(y1);
+	 * Simplifies creating lines
+	 * @param line
+	 * @param x0- starting x
+	 * @param y0- starting y
+	 * @param x1- ending x
+	 * @param y1- ending y
+	 */
+	public static void initLine(Line line, Float x0, Float y0, Float x1, Float y1)
+	{
+		line.setStartX(x0);
+		line.setStartY(y0);
+		line.setEndX(x1);
+		line.setEndY(y1);
+	}
 }
