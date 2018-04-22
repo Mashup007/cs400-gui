4cfc7187f9b3e653d0418280a1288463a855189e
Mashup007 <37002449+Mashup007@users.noreply.github.com>
2018-04-22 18:03:46 -0500

    submit button finished

--- src/application/Main.java
+++ src/application/Main.java
@@ -68,79 +68,74 @@ public class Main extends Application {
 		Scene scene = new Scene(bracket_border, win_width, win_height);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-<<<<<<< HEAD
-	
-
-=======
 		
 		
 		
-		//Creating bracket
-		Path bracket = new Path();
-		
-		//Setting starting point of path
-		MoveTo start = new MoveTo();
-		start.setX(1050);
-		start.setY(300);
-		
-		//Creating winner line
-		HLineTo winner = new HLineTo();
-		winner.setX(1150);
-		
-		bracket.getElements().add(start);
-        bracket.getElements().add(winner);
-        
-		//Creating semifinals
-		MoveTo move = new MoveTo();
-		move.setX(1050);
-		move.setY(400);
-		
-		VLineTo vline = new VLineTo();
-		vline.setY(200);
-		
-		bracket.getElements().add(move);
-		bracket.getElements().add(vline);
-		
-		HLineTo hline = new HLineTo();
-		hline.setX(950);
-		
-		bracket.getElements().add(hline);
-		bracket.getElements().add(move);
-		bracket.getElements().add(hline);
-		
-		move.setY(475);
-		vline.setY(325);
-		
-		bracket.getElements().add(move);
-		bracket.getElements().add(vline);
-		
-		move.setY(275);
-		vline.setY(125);
-		
-		bracket.getElements().add(move);
-        bracket.getElements().add(vline);
-        
-        hline.setX(850);
-        
-        bracket.getElements().add(hline);
-        
-        move.setY(275);
-        
-        bracket.getElements().add(move);
-        bracket.getElements().add(hline);
-        
-        move.setY(325);
-        
-        bracket.getElements().add(move);
-        bracket.getElements().add(hline);
-        
-        move.setY(475);
-        
-        bracket.getElements().add(move);
-        bracket.getElements().add(hline);
-        
-		
->>>>>>> a7c0c767dc5e5673dab374a4d01e94a0fcc3d147
+//		//Creating bracket
+//		Path bracket = new Path();
+//		
+//		//Setting starting point of path
+//		MoveTo start = new MoveTo();
+//		start.setX(1050);
+//		start.setY(300);
+//		
+//		//Creating winner line
+//		HLineTo winner = new HLineTo();
+//		winner.setX(1150);
+//		
+//		bracket.getElements().add(start);
+//        bracket.getElements().add(winner);
+//        
+//		//Creating semifinals
+//		MoveTo move = new MoveTo();
+//		move.setX(1050);
+//		move.setY(400);
+//		
+//		VLineTo vline = new VLineTo();
+//		vline.setY(200);
+//		
+//		bracket.getElements().add(move);
+//		bracket.getElements().add(vline);
+//		
+//		HLineTo hline = new HLineTo();
+//		hline.setX(950);
+//		
+//		bracket.getElements().add(hline);
+//		bracket.getElements().add(move);
+//		bracket.getElements().add(hline);
+//		
+//		move.setY(475);
+//		vline.setY(325);
+//		
+//		bracket.getElements().add(move);
+//		bracket.getElements().add(vline);
+//		
+//		move.setY(275);
+//		vline.setY(125);
+//		
+//		bracket.getElements().add(move);
+//        bracket.getElements().add(vline);
+//        
+//        hline.setX(850);
+//        
+//        bracket.getElements().add(hline);
+//        
+//        move.setY(275);
+//        
+//        bracket.getElements().add(move);
+//        bracket.getElements().add(hline);
+//        
+//        move.setY(325);
+//        
+//        bracket.getElements().add(move);
+//        bracket.getElements().add(hline);
+//        
+//        move.setY(475);
+//        
+//        bracket.getElements().add(move);
+//        bracket.getElements().add(hline);
+//        
+//		
 	}
 	
 	
