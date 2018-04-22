0d9b19189cf2234b62984e1b425825813e686380
Mashup007 <37002449+Mashup007@users.noreply.github.com>
2018-04-22 16:51:09 -0500

    s

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-
+	
 import javafx.scene.control.Button;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -10,15 +10,10 @@ import javafx.scene.shape.Line;
 
 
 public class Main extends Application {
-<<<<<<< HEAD
-	Button button;
-
-=======
     //To make window dimensions more accessible for future use
 	public int win_width = 1200;
 	public int win_height = 600;
 	
->>>>>>> c9a99fe47c1023be735d43804cbc94b47b317dc6
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -29,44 +24,17 @@ public class Main extends Application {
 		//Creating submit button
 		Button button = new Button();
 		button.setText("Submit");
-<<<<<<< HEAD
-
-		StackPane bracket_layout = new StackPane();
-		bracket_layout.getChildren().add(button);
-
-		Scene scene = new Scene(bracket_layout,1200,700);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-=======
 		
 		BorderPane bracket_layout = new BorderPane();
+		
 		bracket_layout.getChildren().add(button);
 		
 		Scene scene = new Scene(bracket_layout, win_width, win_height);
 		primaryStage.setScene(scene);
 		primaryStage.show();
->>>>>>> c9a99fe47c1023be735d43804cbc94b47b317dc6
 	}
-
+	
 	/**
-<<<<<<< HEAD
-	 * Simplifies creating lines
-	 * @param line
-	 * @param x0- starting x
-	 * @param y0- starting y
-	 * @param x1- ending x
-	 * @param y1- ending y
-	 */
-	public static void initLine(Line line, Float x0, Float y0, Float x1, Float y1)
-	{
-		line.setStartX(x0);
-		line.setStartY(y0);
-		line.setEndX(x1);
-		line.setEndY(y1);
-	}
-}
-=======
      * Simplifies creating lines
      * @param line
      * @param x0- starting x
@@ -82,4 +50,3 @@ public class Main extends Application {
         line.setEndY(y1);
     }
 }
->>>>>>> c9a99fe47c1023be735d43804cbc94b47b317dc6
