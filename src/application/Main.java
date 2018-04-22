4a35e8b9dd1590cb4b9d1f0fec995428476a47c5
Mashup007 <37002449+Mashup007@users.noreply.github.com>
2018-04-22 17:31:41 -0500

    a

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.*;
 
@@ -27,53 +28,16 @@ public class Main extends Application {
 		
 		//Creating the general layout
 		BorderPane bracket_border = new BorderPane();
-		bracket_border.setBottom(button);
 		
-		//Add the button to the pane
-		bracket_border.getChildren().add(button);
+		//Set up the submit botton
+		HBox hbox_bottom = new HBox();
 		
-		//Set up the scene
+		bracket_border.setBottom(hbox_bottom);
+		
+		//Set up the scene(framework)
 		Scene scene = new Scene(bracket_border, win_width, win_height);
 		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		
-		
-		//Creating bracket
-		Path bracket = new Path();
-		
-		//Setting starting point of path
-		MoveTo start = new MoveTo();
-		start.setX(1050);
-		start.setY(300);
-		
-		//Creating winner line
-		HLineTo winner = new HLineTo();
-		winner.setX(1150);
-		
-		bracket.getElements().add(start);
-        bracket.getElements().add(winner);
-        
-		//Creating semifinals
-		MoveTo move = new MoveTo();
-		move.setX(1050);
-		move.setY(350);
-		
-		VLineTo vline = new VLineTo();
-		vline.setY(250);
-		
-		bracket.getElements().add(move);
-		bracket.getElements().add(vline);
-		
-		HLineTo hline = new HLineTo();
-		hline.setX(950);
-		
-		bracket.getElements().add(hline);
-		bracket.getElements().add(move);
-		bracket.getElements().add(hline);
-		
-		bracket_layout.getChildren().add(bracket);
-		
+		primaryStage.show();	
 	}
 	
 	/**
