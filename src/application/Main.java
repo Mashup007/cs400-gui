8d9c2f50057a589e19fd78527235fd0d6c824705
Mashup007 <37002449+Mashup007@users.noreply.github.com>
2018-04-22 16:39:12 -0500

    name correction

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	}
 	@Override
 	public void start(Stage primaryStage)throws Exception{
-		primaryStage.setTitle("Tornament");
+		primaryStage.setTitle("Tournament");
 		button = new Button();
 		button.setText("Submit");
 		
