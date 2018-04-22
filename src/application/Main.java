ce7285f3cf8b8b3af527cc6f56e66affd7182c55
Mashup007 <37002449+Mashup007@users.noreply.github.com>
2018-04-22 16:34:54 -0500

    a
    a

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,31 @@
 package application;
 	
+import javafx.scene.control.Button;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	Button button;
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	@Override
+	public void start(Stage primaryStage)throws Exception{
+		primaryStage.setTitle("Tornament");
+		button = new Button();
+		button.setText("Submit");
+		
+		StackPane bracket_layout = new StackPane();
+		bracket_layout.getChildren().add(button);
+		
+		Scene scene = new Scene(bracket_layout,1200,700);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 }
