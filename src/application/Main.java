18e55bd42217192c35e85b9ae2ee6d8eef1c64d8
Tarun Mandalapu <t.mandalapu@gmail.com>
2018-04-22 16:39:18 -0500

    Added initLine

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	}
 	@Override
 	public void start(Stage primaryStage)throws Exception{
-		primaryStage.setTitle("Tornament");
+		primaryStage.setTitle("Tournament");
 		button = new Button();
 		button.setText("Submit");
 		
@@ -28,4 +28,19 @@ public class Main extends Application {
 		primaryStage.show();
 		
 	}
+	
+	/**
+     * Simplifies creating lines
+     * @param line
+     * @param x0- starting x
+     * @param y0- starting y
+     * @param x1- ending x
+     * @param y1- ending y
+     */
+    public static void initLine(Line line, Float x0, Float y0, Float x1, Float y1)
+    {
+        line.setStartX(x0);
+        line.setStartY(y0);
+        line.setEndX(x1);
+        line.setEndY(y1);
 }
