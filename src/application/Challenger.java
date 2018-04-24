f5634b8ed013997f34ba7f80eb16d9b88fdc77af
Matt Zimmers <mzimmers@wisc.edu>
2018-04-24 05:51:37 -0500

    Re-organized entirely

--- src/application/Challenger.java
+++ src/application/Challenger.java
@@ -1,10 +1,53 @@
 package application;
 
+import java.nio.file.Path;
+import java.util.Observable;
+import java.util.Scanner;
+
+import javax.swing.event.ChangeListener;
+
+import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
 public class Challenger {
     private String teamName;
     private Integer teamRank;
     private Integer teamScore;
     private boolean teamInGame = true;
+    private Challenger opposition;
+    private TextField scoreField;
+    private Label nameLabel;
+    private int currRound;
     
     /**
      * Basic constructor, initializes teamName, teamRank, and teamInGame.
@@ -13,10 +56,72 @@ public class Challenger {
      */
     public Challenger(String name, Integer rank)
     {
+        teamScore = -1;
         this.teamName = name;
         this.teamRank = rank;
+        currRound = 1;
     }
     
+    public HBox fillHBox(HBox hbox) {
+
+        nameLabel = new Label();
+        nameLabel.setAlignment(Pos.CENTER);
+        nameLabel.setText(teamName);
+        
+        scoreField = new TextField();
+        scoreField.setPromptText("Score");
+        scoreField.setMaxSize(60, 30);
+        scoreField.setMinSize(60, 30);
+        scoreField.textProperty().addListener((obs, oldText, newText) -> {
+            try {
+                if (newText == "") teamScore = 0;
+                else teamScore = Integer.parseInt(newText);
+            } catch (Exception e) {}
+        });
+        
+        hbox.getChildren().addAll(nameLabel, scoreField);
+        
+        return hbox;
+        
+    }
+    
+    public VBox fillVBox(VBox vbox) {
+        nameLabel = new Label();
+        nameLabel.setAlignment(Pos.CENTER);
+        nameLabel.setText(teamName);
+        
+        scoreField = new TextField();
+        scoreField.setPromptText("Score");
+        scoreField.setMaxSize(60, 30);
+        scoreField.setMinSize(60, 30);
+        scoreField.textProperty().addListener((obs, oldText, newText) -> {
+            try {
+                if (newText == "") teamScore = 0;
+                else teamScore = Integer.parseInt(newText);
+            } catch (Exception e) {}
+        });
+        
+        vbox.getChildren().addAll(nameLabel, scoreField);
+        
+        return vbox;
+    }
+    
+    public int getCurrRound() {
+        return currRound;
+    }
+
+    public void setCurrRound(int currRound) {
+        this.currRound = currRound;
+    }
+
+    public Challenger getOpposition() {
+        return opposition;
+    }
+
+    public void setOpposition(Challenger opposition) {
+        this.opposition = opposition;
+    }
+
     /**
      * @return teamName
      */
@@ -29,14 +134,6 @@ public class Challenger {
      */
     public Integer getTeamRank() {
         return teamRank;
-    }
-
-    /**
-     * Change team rank as tournament proceeds
-     * @param teamRank: new team ranking
-     */
-    public void setTeamRank(Integer teamRank) {
-        this.teamRank = teamRank;
     }
 
     /**
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.awt.event.MouseEvent;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Observable;
+import java.util.Random;
 import java.util.Scanner;
 
 import javafx.application.Application;
@@ -13,6 +16,7 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
@@ -35,325 +39,219 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class Main extends Application {
 	//To make window dimensions more accessible for future use
 	public int win_width = 1200;
 	public int win_height = 600;
 	private int numTeams = 16;
+	private int rounds;
 	public static int curLine = 0;
+	private ArrayList<ArrayList<Challenger>> challengers;
+	private ArrayList<VBox> columns;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 	@Override
-	public void start(Stage primaryStage)throws Exception{
-		primaryStage.setTitle("Tournament");
+	public void start(Stage primaryStage) throws Exception {
+	    primaryStage.setTitle("Tournament");
+        rounds = (int)(Math.log10(numTeams)/Math.log10(2));
 
-		//Creating the general layout (Using BorderPane)
-		BorderPane bracket_layout = new BorderPane();
-		
-		//Set up the title
-		Label title = new Label("Tournament");
-		title.setAlignment(Pos.CENTER);
-		title.setMinHeight(25);
-		title.setMaxWidth(1500);
-		bracket_layout.setTop(title);
-		
-		
-		//Creating submit button
-		Button button = new Button();
-		button.setText("Submit");
+        //Creating the general layout (Using BorderPane)
+        BorderPane bracket_layout = new BorderPane();
+        
+        //Set up the title
+        Label title = new Label("Tournament");
+        title.setAlignment(Pos.CENTER);
+        title.setMinHeight(30);
+        title.setMaxHeight(30);
+        title.setMaxWidth(win_width);
+        bracket_layout.setTop(title);
+        
+        //Creating submit button
+        Button submit = new Button();
+        submit.setText("Submit");
+        submit.setMinSize(80, 20);
+        submit.setOnMouseClicked(e -> { submitClicked(); });
+        HBox hbox_bottom = new HBox();
+        hbox_bottom.getChildren().add(submit);
+        hbox_bottom.setAlignment(Pos.TOP_CENTER);
+        hbox_bottom.setMinHeight(40);
+        hbox_bottom.setMaxHeight(40);
+        bracket_layout.setBottom(hbox_bottom);
 
-		//Set up the submit botton
-		HBox hbox_bottom = new HBox();
-		hbox_bottom.getChildren().add(button);
-		hbox_bottom.setAlignment(Pos.CENTER);
-		hbox_bottom.setPadding(new Insets(20));
-		bracket_layout.setBottom(hbox_bottom);
+        //////////////Create Challenger Objects/////////////////
 
-		//////////////Set up the challengers/////////////////
-		VBox r1Left = new VBox(100);
-		VBox r2Left = new VBox(220);
-		VBox r1Right = new VBox(100);
-		VBox r2Right= new VBox(220);
-		VBox r3final = new VBox(50);
-
-		HBox hbox1 = new HBox(10);
-		Label label1 = new Label();
-		label1.setAlignment(Pos.CENTER);
-		label1.setMinHeight(25);
-		label1.setText("Team1");
-
-		TextField score1 = new TextField();
-		score1.setMaxHeight(20); score1.setMaxWidth(60);
-		score1.setPromptText("Score");
-		score1.setFocusTraversable(false);
-		hbox1.getChildren().addAll(label1, score1);
-
-		HBox hbox2 = new HBox(10);
-		Label label2 = new Label();
-		label2.setAlignment(Pos.CENTER);
-		label2.setMinHeight(25);
-		label2.setText("Team2");
-
-		TextField score2 = new TextField();
-		score2.setMaxHeight(20); score2.setMaxWidth(60);
-		score2.setPromptText("Score");
-		score2.setFocusTraversable(false);
-		hbox2.getChildren().addAll(label2, score2);
-
-		HBox hbox3 = new HBox(10);
-		Label label3 = new Label();
-		label3.setAlignment(Pos.CENTER);
-		label3.setMinHeight(25);
-		label3.setText("Team3");
-
-		TextField score3 = new TextField();
-		score3.setMaxHeight(20); score3.setMaxWidth(60);
-		score3.setPromptText("Score");
-		score3.setFocusTraversable(false);
-		hbox3.getChildren().addAll(label3, score3);
-
-		HBox hbox4 = new HBox(10);
-		Label label4 = new Label();
-		label4.setAlignment(Pos.CENTER);
-		label4.setMinHeight(25);
-		label4.setText("Team4");
-
-		TextField score4 = new TextField();
-		score4.setMaxHeight(20); score4.setMaxWidth(60);
-		score4.setPromptText("Score");
-		score4.setFocusTraversable(false);
-		hbox4.getChildren().addAll(label4, score4);
-
-		HBox hbox5 = new HBox(10);
-		Label label5 = new Label();
-		label5.setAlignment(Pos.CENTER);
-		label5.setMinHeight(25);
-		label5.setText("Team5");
-
-		TextField score5 = new TextField();
-		score5.setMaxHeight(20); score5.setMaxWidth(60);
-		score5.setPromptText("Score");
-		score5.setFocusTraversable(false);
-		hbox5.getChildren().addAll(label5, score5);
-
-		HBox hbox6 = new HBox(10);
-		Label label6 = new Label();
-		label6.setAlignment(Pos.CENTER);
-		label6.setMinHeight(25);
-		label6.setText("Team6");
-
-		TextField score6 = new TextField();
-		score6.setMaxHeight(20); score6.setMaxWidth(60);
-		score6.setPromptText("Score");
-		score6.setFocusTraversable(false);
-		hbox6.getChildren().addAll(label6, score6);
-
-		HBox hbox7 = new HBox(10);
-		Label label7 = new Label();
-		label7.setAlignment(Pos.CENTER);
-		label7.setMinHeight(25);
-		label7.setText("Team7");
-
-		TextField score7 = new TextField();
-		score7.setMaxHeight(20); score7.setMaxWidth(60);
-		score7.setPromptText("Score");
-		score7.setFocusTraversable(false);
-		hbox7.getChildren().addAll(label7, score7);
-
-		HBox hbox8 = new HBox(10);
-		Label label8 = new Label();
-		label8.setAlignment(Pos.CENTER);
-		label8.setMinHeight(25);
-		label8.setText("Team8");
-
-		TextField score8 = new TextField();
-		score8.setMaxHeight(20); score8.setMaxWidth(60);
-		score8.setPromptText("Score");
-		score8.setFocusTraversable(false);
-		hbox8.getChildren().addAll(label8, score8);
-
-		r1Right.getChildren().addAll(hbox5,hbox6,hbox7,hbox8);
-		//add evertying into the current vbox
-
-		bracket_layout.setRight(r1Right);
-		r1Right.setAlignment(Pos.CENTER);
-		r1Right.setPadding(new Insets(0,100,0,0));
-
-		////////////setting up for round 2/////////////////
-		///////left side////////////
-		HBox hbox1_r2 = new HBox();
-		Label label1_r2 = new Label();
-		label1_r2.setAlignment(Pos.CENTER);
-		label1_r2.setMinHeight(25);
-		label1_r2.setText("Team1");
-
-		TextField score1_r2 = new TextField();
-		score1_r2.setMaxHeight(20); score1_r2.setMaxWidth(60);
-		score1_r2.setPromptText("Score");
-		score1_r2.setFocusTraversable(false);
-		hbox1_r2.getChildren().addAll(label1_r2, score1_r2);
-
-		HBox hbox3_r2 = new HBox();
-		Label label3_r2 = new Label();
-		label3_r2.setAlignment(Pos.CENTER);
-		label3_r2.setMinHeight(25);
-		label3_r2.setText("Team3");
-
-		TextField score3_r2 = new TextField();
-		score3_r2.setMaxHeight(20); score3_r2.setMaxWidth(60);
-		score3_r2.setPromptText("Score");
-		score3_r2.setFocusTraversable(false);
-		hbox3_r2.getChildren().addAll(label3_r2, score3_r2);
-		//////right side/////
-		HBox hbox5_r2 = new HBox(10);
-		Label label5_r2 = new Label();
-		label5_r2.setAlignment(Pos.CENTER);
-		label5_r2.setMinHeight(25);
-		label5_r2.setText("Team5");
-
-		TextField score5_r2 = new TextField();
-		score5_r2.setMaxHeight(20); score5_r2.setMaxWidth(60);
-		score5_r2.setPromptText("Score");
-		score5_r2.setFocusTraversable(false);
-		hbox5_r2.getChildren().addAll(label5_r2, score5_r2);
-		
-		HBox hbox7_r2 = new HBox(10);
-		Label label7_r2 = new Label();
-		label7_r2.setAlignment(Pos.CENTER);
-		label7_r2.setMinHeight(25);
-		label7_r2.setText("Team7");
-
-		TextField score7_r2 = new TextField();
-		score7_r2.setMaxHeight(20); score7_r2.setMaxWidth(60);
-		score7_r2.setPromptText("Score");
-		score7_r2.setFocusTraversable(false);
-		hbox7_r2.getChildren().addAll(label7_r2, score7_r2);
-		
-		HBox hbox1_r3 = new HBox(10);
-		Label label1_r3 = new Label();
-		label1_r3.setAlignment(Pos.CENTER);
-		label1_r3.setMinHeight(25);
-		label1_r3.setText("Team1");
-
-		TextField score1_r3 = new TextField();
-		score1_r3.setMaxHeight(20); score1_r3.setMaxWidth(60);
-		score1_r3.setPromptText("Score");
-		score1_r3.setFocusTraversable(false);
-		hbox1_r3.getChildren().addAll(label1_r3, score1_r3);
-		
-		HBox hbox7_r3 = new HBox(10);
-		Label label7_r3 = new Label();
-		label7_r3.setAlignment(Pos.CENTER);
-		label7_r3.setMinHeight(25);
-		label7_r3.setText("Team7");
-
-		TextField score7_r3 = new TextField();
-		score7_r3.setMaxHeight(20); score7_r3.setMaxWidth(60);
-		score7_r3.setPromptText("Score");
-		score7_r3.setFocusTraversable(false);
-		hbox7_r3.getChildren().addAll(label7_r3, score7_r3);
-		////////////setting up for round 2 done/////////////
-
-		HBox hbox9 = new HBox(10);
-		hbox9.getChildren().addAll(label1_r2,score1_r2);
-
-		HBox hbox10 = new HBox(10);
-		hbox10.getChildren().addAll(label3_r2,score3_r2);
-		
-		HBox hbox11 = new HBox(10);
-		hbox11.getChildren().addAll(label5_r2,score5_r2);
-		
-		HBox hbox12 = new HBox(10);
-		hbox12.getChildren().addAll(label7_r2,score7_r2);
-		
-		HBox hbox13 = new HBox(10);
-		hbox13.getChildren().addAll(label1_r3,score1_r3);
-		
-		HBox hbox14 = new HBox(10);
-		hbox14.getChildren().addAll(label7_r3,score7_r3);
-		
-		/////vobox for round2_right
-		r2Right.getChildren().addAll(hbox11,hbox12);
-		
-		////////vbox for round2_left
-		r2Left.getChildren().addAll(hbox9,hbox10);
-		
-		////////vbox for final round
-		r3final.getChildren().addAll(hbox13,hbox14);
-		r3final.setPadding(new Insets(100,100,0,100));
-		
-		HBox hboxcenter = new HBox();
-		hboxcenter.getChildren().addAll(r2Left,r3final,r2Right);
-		bracket_layout.setCenter(hboxcenter);
-		hboxcenter.setPadding(new Insets(110,0,0,100));
-
-		//////vbox for round1_left
-		r1Left.getChildren().addAll(hbox1,hbox2,hbox3,hbox4);
-		bracket_layout.setLeft(r1Left);
-		r1Left.setAlignment(Pos.CENTER);
-		r1Left.setPadding(new Insets(0,0,0,100));
-		////////////////Set Challengers Done/////////////////////////
-		
-		//Set up the scene(framework)
-		Scene scene = new Scene(bracket_layout, win_width, win_height);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-
-		
-
-		//Holds all the lines needed to create bracket
-		Line[] lines = new Line[getNumLines(numTeams)];
-		int rounds = 3;
-		int line_height = (win_height)/2;
-		int line_width = (win_width)/(rounds*2);
-		//Center line
-		lines[0] = new Line(500, 300, 700, 300); 
-		curLine++;
-		lines = addBrack(lines, 700, 300, line_height, line_width, rounds-1);
-		lines = addBrack(lines, 500, 300, line_height, -line_width, rounds-1);
-		
-		for(Line line: lines) {
-		    if(line == null)
-		    {
-		        break;
-		    }
-		    bracket_layout.getChildren().add(line);
-		}
-		
-	}
-	private static Line[] addBrack(Line[] lines, int x, int y, int line_height, int line_width, int rounds) {
-	    int bot = y+line_height/2;
-	    int top = y-line_height/2;
-	    int h_end = x+line_width;
-	    //vert
-	    lines[curLine] = new Line(x, top, x, bot);
-	    lines[curLine+1] = new Line(x, top, h_end, top);
-	    lines[curLine+2] = new Line(x, bot, h_end, bot);
-	    curLine += 3;
-	    if(rounds != 1) {
-	        lines = addBrack(lines, h_end, bot, line_height/2, line_width, rounds-1);
-	        return addBrack(lines, h_end, top, line_height/2, line_width, rounds-1);
-	    }
-	    else {
-	        return lines;
-	    }
-	}
-    private static int getNumLines(int numTeams)
-    {
-        int result;
-        if(numTeams == 2)
-        {
-            return 1;
+        challengers = new ArrayList<ArrayList<Challenger>>();
+        for (int i = 0; i < rounds; i++) {
+            challengers.add(new ArrayList<Challenger>());
         }
-        else
-        {
-            return 3*(numTeams/2)+getNumLines(numTeams/2);
+        
+        challengers.set(0, seed());
+        
+        // Fill proceeding slots with nulls
+        for (int i = 1; i < rounds; i++) {
+            for (int j = 0; j < numTeams/Math.pow(2, i); j++) {
+                challengers.get(i).add(null);
+            }
+        }
+        
+        // Split Center by Horizontally Ordered VBoxes
+        HBox central = new HBox(); // VBox columns stored in here
+        
+        double colSpace = (double)(win_width)/(rounds*2-1);
+        columns = new ArrayList<VBox>();
+        for (int i = 0; i < rounds*2-1; i++) {
+            VBox newCol = new VBox(); 
+            // Fit to window and set constant height
+            newCol.setMaxSize(colSpace, 530);
+            newCol.setMinSize(colSpace, 530);
+            newCol.setAlignment(Pos.CENTER);          
+            columns.add(newCol);
+        }
+        
+        // Add appropriate number of HBoxes to end columns containing Challenger info
+        int challengerIndex = 0;
+        for (int i = 0; i < columns.size(); i++) {
+            int numBoxes = (int)Math.pow(2,Math.abs(rounds-1-i));
+            ArrayList<HBox> boxes = new ArrayList<HBox>();
+            for (int j = 0; j < numBoxes; j++) {
+                if (numBoxes != 1) {
+                    HBox newHBox = new HBox(10);
+                    newHBox.setAlignment(Pos.CENTER);
+                    newHBox.setMinSize(colSpace, (double)530/numBoxes);
+                    newHBox.setMaxSize(colSpace, (double)530/numBoxes);
+                    if (i == 0 || i == columns.size()-1) {
+                        newHBox = challengers.get(0).get(challengerIndex).fillHBox(newHBox);
+                        challengerIndex++;
+                    } 
+                    boxes.add(newHBox);
+                } else if (numTeams == 2) {
+                    columns.set(i, challengers.get(0).get(challengerIndex).fillVBox(columns.get(i)));
+                    challengerIndex++;
+                    columns.set(i, challengers.get(0).get(challengerIndex).fillVBox(columns.get(i)));
+                    columns.get(i).setSpacing(50);
+                }
+            }
+            if (boxes.size() != 0) columns.get(i).getChildren().addAll(boxes);
+        }
+        // Add finished columns to central layout
+        central.getChildren().addAll(columns);
+        bracket_layout.setCenter(central);
+        
+        // Set up the scene(framework)
+        Scene scene = new Scene(bracket_layout, win_width, win_height);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        //////////////// Creates Lines Independent of (H/V)Boxes ////////////////////////
+        //Holds all the lines needed to create bracket
+        Line[] lines = new Line[getNumLines(numTeams)];
+        double line_height = (win_height-70)/2; // Fit to center height
+        double line_width = (win_width)/(rounds*2-1); // Fit to window width
+        //Center line
+        lines[0] = new Line(600-line_width/2, 312, 600+line_width/2, 310); 
+        if (lines.length != 1) {
+            lines = addBrack(lines, 600+line_width/2, 312, line_height, line_width, rounds-1);
+            lines = addBrack(lines, 600-line_width/2, 312, line_height, -line_width, rounds-1);
+        }
+        
+        for(Line line: lines) {
+            if(line == null)
+            {
+                break;
+            }
+            bracket_layout.getChildren().add(line);
+        }
+	} 
+	
+   private static Line[] addBrack(Line[] lines, double x, double y, double line_height, double line_width, int rounds) {
+        double bot = y+line_height/2;
+        double top = y-line_height/2;
+        double h_end = x+line_width;
+        //vert
+        lines[++curLine] = new Line(x, top, x, bot);
+        lines[++curLine] = new Line(x, top, h_end, top);
+        lines[++curLine] = new Line(x, bot, h_end, bot);
+        if (rounds != 1) {
+            lines = addBrack(lines, h_end, bot, line_height/2, line_width, rounds-1);
+            return addBrack(lines, h_end, top, line_height/2, line_width, rounds-1);
+        } else {
+            return lines;
         }
     }
-
-
+   
+    private static int getNumLines(int numTeams) {
+        if (numTeams == 2) return 1;
+        else return 3*(numTeams/2)+getNumLines(numTeams/2);
+    }
+	
+	private ArrayList<Challenger> seed() {
+	    ArrayList<Challenger> tempChallengers = new ArrayList<Challenger>();
+	    ArrayList<Challenger> startingChallengers = new ArrayList<Challenger>();
+	    for (int i = 0; i < numTeams; i++) {
+	        tempChallengers.add(new Challenger(randName(), i + 1));
+	    }
+	    // seed opposition
+	    for (int i = 0; i < numTeams/2; i++) {
+	        tempChallengers.get(i).setOpposition(tempChallengers.get(numTeams-1-i));
+	        tempChallengers.get(numTeams-1-i).setOpposition(tempChallengers.get(i));
+	        
+	        startingChallengers.add(tempChallengers.get(i));
+	        startingChallengers.add(tempChallengers.get(i).getOpposition());
+	    }
+	    return startingChallengers;
 	}
+	
+	private String randName() {
+	    Random rand = new Random();
+	    String[] adjectives = new String[] { "Blue", "Red", "Green", "Yellow", 
+	                                        "Fighting", "Big", "Little", "Alpha", 
+	                                        "Mean", "Gruesome", "Unstoppable", 
+	                                        "Crazy", "Amazing", "Incredible",
+	                                        "Fantastic" };
+	    String[] nouns = new String[] { "Devils", "Saints", "Apples", "Machines",
+	                                    "Tigers", "Penguins", "Jets", "Patriots",
+	                                    "Darts", "Vikings", "Colts", "Panthers",
+	                                    "Eagles", "Ravens", "Hawks", "Angels",
+	                                    "Chickens", "Roosters", "Bananas", "Warriors" };
+	    
+	    return adjectives[rand.nextInt(adjectives.length)] + " " + nouns[rand.nextInt(nouns.length)];
+	}
+	
+	private void submitClicked() {
+	    int currRound = 1;
+	    for (ArrayList<Challenger> a : challengers) {   
+	        for (int i = 0; i < a.size(); i+=2) {
+	            Challenger t1 = a.get(i);
+	            Challenger t2 = a.get(i+1);
+	            if (t1 != null && t2 != null && t1.getTeamScore() != t2.getTeamScore()
+	                    && t1.isTeamInGame() && t2.isTeamInGame() 
+	                    && t1.getTeamScore() != -1 && t2.getTeamScore() != -1
+	                    && t1.getCurrRound() == currRound && t2.getCurrRound() == currRound) {
+	                Challenger winner = t1;
+	                if (t1.getTeamScore() < t2.getTeamScore()) winner = t2;
+	                //tournamentAdvance(currRound, winner, i);
+	            }
+	        }
+	        currRound++;
+	    }
+	}
+	
+	private void tournamentAdvance(int currRound, Challenger winner, int index) {
+	    HBox modify;
+	    HBox destination;
+	    int numTeamsinRound = numTeams/(int)Math.pow(2, currRound-1);
+	    
+	    // winner on left side of bracket
+	    if (index < numTeamsinRound/2) {
+	        for (int i = 0; i < numTeamsinRound/2;i++) {
+	            if (((Parent) columns.get(currRound-1).getChildren().get(i)))
+	        }
+	    }
+	}
+
+}
